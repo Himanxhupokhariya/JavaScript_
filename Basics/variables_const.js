3642cf337549adaabe6e8519d587671f8e897f4f
Himanshu_Pokhariya <156579564+Himanxhupokhariya@users.noreply.github.com>
2024-08-25 19:53:14 +0000

    config done

---
+++ Basics/variables_const.js
@@ -0,0 +1,13 @@
+const value=122
+let gmail="hp@gmail.com"    //variable
+let no="12345"              //variable
+
+// value=1234
+gmail="hpgggmail"          //variable
+no="1233"                  //variable
+// console.log(value)
+// console.log(no)
+// console.log(gmail)
+
+// If we print all in one then use table. 
+console.table([value,gmail,no])
